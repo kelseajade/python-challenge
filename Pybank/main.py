0e5a289be4d596145b1c99ef677990a5dded6fb6
kelseajade <152021966+kelseajade@users.noreply.github.com>
2024-01-01 15:30:55 -0800

    Update main.py

--- Pybank/main.py
+++ Pybank/main.py
@@ -1,1 +1,16 @@
 
+import os
+
+import csv
+
+budgetdata = os.path.join('..', 'Resources', 'budget_data.csv')
+
+with open(budgetdata) as budgetdata:
+
+    print(budgetdata)
+
+    csvheader = next(budgetdata)
+
+    print(f"CSV Header: {csvheader}")
+
+
